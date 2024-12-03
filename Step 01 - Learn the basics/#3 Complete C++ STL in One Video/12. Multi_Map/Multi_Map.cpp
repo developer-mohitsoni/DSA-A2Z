6a1a8d7c20e32f908028be5d18f93cbf0c4e0132
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-03 19:08:03 +0530

    Add example demonstrating Multi-Map operations in C++

---
+++ Step 01 - Learn the basics/#3 Complete C++ STL in One Video/12. Multi_Map/Multi_Map.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void explainMultiMap(){
+
+    // everything same as map, only it cans store multiple keys
+    // only mpp[key] cannot be used here
+
+
+
+}
+int main(){
+    // What is Multi-Map in STL
+    // A multi-map is an associative container that contains equivalent elements and their corresponding values
+    // A multi-map is a container that stores elements formed by pairs where each element has a key value and multiple mapped values. It allows duplicate keys but not duplicate elements.
+
+    /*
+        A multimap is a container that stores unique keys and allows multiple values for each key. It is similar to std::map but unlike std::map, it can have more than one value
+    */
+
+
+    // List of the Operations perfromed in a Multi-Map (Same as Map)
+
+
+    explainMultiMap();
+
+    return 0;
+}
