fb0dfffde2cad1f5ff4d7bbaac8e324e45e3e330
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-03 10:11:57 +0530

    Add example program demonstrating the use of do while loops in C++

---
+++ Step 01 - Learn the basics/#1 Things to know/8. Do While Loop/Do_While_Loop.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int i=2;
+    // Do while loop
+    do{
+        cout<<"Mohit "<<i<<endl;
+
+        i+=1;
+
+    }while(i<=1);
+
+    cout<<"Value after terminate the loop: "<<i<<endl;
+
+    // Step 1:- Initialization mandatory before do while loop:
+    // Step 2:- Printing what inside (do) syntax
+    // Step 3:- Increment / Decreemnt
+    // Step 4:- Condition Checking
+
+    /*NOTE:- In this case do while printing the output once even if the the condition is wrong.*/
+
+    return 0;
+}
