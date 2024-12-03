f03cbf902ab83e13dec5abd08d657e275b41129b
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-03 09:44:53 +0530

    Add example program demonstrating various data types in C++

---
+++ Step 01 - Learn the basics/#1 Things to know/2. Data Types/Data_Types.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // int
+    int x = 10;
+    cout << "Value of x: " << x << endl;
+    cout << "Size of int: " << sizeof(int) << endl
+         << endl;
+
+    // long
+    long y = 15;
+    cout << "Value of y: " << y << endl;
+    cout << "Size of long: " << sizeof(long) << endl
+         << endl;
+
+    // long long
+    long long z = 150000000;
+    cout << "Value of z: " << z << endl;
+    cout << "Size of long long: " << sizeof(long long) << endl
+         << endl;
+
+    // float
+    float p = 5.6;
+    cout << "Value of p: " << p << endl;
+    cout << "Size of float: " << sizeof(float) << endl
+         << endl;
+
+    // double
+    double q = 5;
+    cout << "Value of q: " << q << endl;
+    cout << "Size of double: " << sizeof(double) << endl
+         << endl;
+
+    return 0;
+}
