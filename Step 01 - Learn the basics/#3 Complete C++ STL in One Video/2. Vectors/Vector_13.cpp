7fa4dd0e7128d0d83923889930c28f5e8a95f9b1
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-03 18:28:49 +0530

    Add 13th example demonstrating vector size in C++

---
+++ Step 01 - Learn the basics/#3 Complete C++ STL in One Video/2. Vectors/Vector_13.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    // Size of the vector:-
+
+    vector<int>v13;
+    v13.push_back(6);
+    v13.push_back(12);
+    v13.push_back(18);
+    v13.push_back(24);
+    v13.push_back(30);
+
+    // {6,12,18,24,30}
+    cout<<v13.size(); // Output:- 5
+    
+    return 0;
+}
