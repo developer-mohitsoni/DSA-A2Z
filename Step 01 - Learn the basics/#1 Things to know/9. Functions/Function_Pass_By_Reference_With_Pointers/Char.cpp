e7d59b7f15df2474988bf5c62d3e92ae3840ec79
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-03 11:03:12 +0530

    Add example program demonstrating function pass by reference with char using pointers in C++

---
+++ Step 01 - Learn the basics/#1 Things to know/9. Functions/Function_Pass_By_Reference_With_Pointers/Char.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+// Functions are set of code which performs something for you.
+// Functions are used to modularise code.
+// Functions are used to increase readibility.
+// Functions are used to use same code multiple time.
+
+
+// Pass By Reference with Pointers
+void doSomething(char *ch){ /*Original value comes*/
+
+    /*These all are modification in the original value*/
+    cout<<*ch<<endl;
+
+    *ch += 5;
+
+    cout<<*ch<<endl;
+
+    *ch += 5;
+
+    cout<<*ch<<endl;
+}
+
+int main(){
+    char ch = 'a';
+
+    doSomething(&ch); /*original number address goes*/
+
+    cout<<ch<<endl; /*Now it print 'k'*/
+    return 0;
+}
