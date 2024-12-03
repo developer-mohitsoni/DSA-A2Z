dd842b0dce45b6c53ec90b4a8afc145352d608f0
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-03 19:06:50 +0530

    Add 1st example demonstrating unordered_set operations in C++

---
+++ Step 01 - Learn the basics/#3 Complete C++ STL in One Video/10. Universal_Set/Unorder_Set.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void explainUSet(){
+    unordered_set<int> ust;
+
+    // lower_bound() and upper_bound() function
+    // does not works, rest all functions are same as Set,
+    // it does not store in any particular order, it has a
+    // better complexity than set in most cases, except some
+    // when collision happens
+
+    // Most cases T.C is O(1)
+
+    // Worst Case happens rarely in unordered_set then it's T.C is O(N)
+
+}
+int main(){
+    // What is UnorderedSet in STL
+    // It is an associative container that contains key value pairs.
+    // The data items are stored in the form of nodes. Each node consists  
+    // of a key-value pair. There is no particular order in which elements  
+    // are placed into the containers, making them ideal for applications  
+    // where fast access to individual elements is necessary.
+
+
+
+    // List of the Operations perfromed in a UniversalSet (Same as all Set Operations except lower_bound() and upper_bound())
+
+    explainUSet();
+
+    return 0;
+}
