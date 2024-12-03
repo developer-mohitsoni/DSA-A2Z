8338124731c11a2f2fea213fa85d1d42cc8ffaeb
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-03 09:46:21 +0530

    Add example program for string input handling in C++

---
+++ Step 01 - Learn the basics/#1 Things to know/2. Data Types/Data_Types_3.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    string s1;
+    string s2;
+
+    cin>>s1>>s2;
+
+    cout<<s1 <<" "<<s2; /*Not a right way to print entire string*/
+
+    return 0;
+}
