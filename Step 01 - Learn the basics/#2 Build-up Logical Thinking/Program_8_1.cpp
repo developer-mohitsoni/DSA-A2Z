064af834c9542022e218d27c055ccd90b615b8b2
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-03 17:24:52 +0530

    Add pattern 26: Inverted Number Pyramid Pattern

---
+++ Step 01 - Learn the basics/#2 Build-up Logical Thinking/Program_8_1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int row;
+
+    cout << "Enter The Number of Rows: " << endl;
+    cin >> row;
+
+    cout << "Print the Pattern:- " << endl;
+
+    for (int i = 0; i < row; i++)
+    {
+        // spaces
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        // stars
+        for (int j = 0; j < (2 * row) - (2 * i + 1); j++)
+        {
+            cout << j + 1;
+        }
+        // spaces
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
