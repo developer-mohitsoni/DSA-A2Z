216ccaee92b73a3f182a82bb20c36eb338fea33e
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-03 09:46:06 +0530

    Add example program for string input and output in C++

---
+++ Step 01 - Learn the basics/#1 Things to know/2. Data Types/Data_Types_2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // string
+
+    string s;
+    cin >> s;
+
+    cout << s; /*Only Shows the string until first space not encounter*/
+
+    return 0;
+}
