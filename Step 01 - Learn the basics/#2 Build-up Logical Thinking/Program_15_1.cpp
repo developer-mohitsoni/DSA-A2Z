0ce5e8e6235e19f13b6fd370d10e286d50d274a4
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-03 17:41:57 +0530

    Add pattern 32: Alphabetical Pyramid Pattern

---
+++ Step 01 - Learn the basics/#2 Build-up Logical Thinking/Program_15_1.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int row;
+
+    cout << "Enter The Number of Rows: " << endl;
+    cin >> row;
+
+    cout << "Print the Pattern:- " << endl;
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < row - i; j++)
+        {
+
+            // for every column
+            char alpha = i + 'A';
+
+            cout << alpha << " ";
+        }
+        cout << endl;
+    }
+
+    /*We can also dones as:- */
+    /*
+    for(int i=0; i<row; i++){
+        for(char ch='A'; ch<='A'+(row-i-1); ch++){
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+    */
+}
