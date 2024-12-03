72e41fb4eec378863b6640d747a5e5f9b7668b75
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-03 17:18:40 +0530

    Add pattern 24: Pyramid Number Pattern

---
+++ Step 01 - Learn the basics/#2 Build-up Logical Thinking/Program_7_1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int row;
+
+    cout << "Enter The Number of Rows: " << endl;
+    cin >> row;
+
+    cout << "Print the Pattern:- " << endl;
+
+    for (int i = 0; i < row; i++)
+    {
+        // spaces
+        for (int j = 0; j < row - i - 1; j++)
+        {
+            cout << " ";
+        }
+        // stars
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << i + 1;
+        }
+        // spaces
+        for (int j = 0; j < row - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
