7926de775fe08a41540e50af6677996ca923c165
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-03 20:52:28 +0530

    Add C++ program to calculate GCD and LCM using optimized approach (Euclidean Algorithm)

---
+++ Step 01 - Learn the basics/#4 Know Basic Maths/9. LCM_or_GCD/LCM_or_GCD.cpp
@@ -0,0 +1,52 @@
+// Optimized Approach (Euclidean Algorithm):-
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int calcGCD(int n, int m)
+{
+
+    while (n > 0 && m > 0)
+    {
+        if (n > m)
+            n = n % m;
+        else
+            m = m % n;
+    }
+
+    if (n == 0)
+        return m;
+    else
+        return n;
+}
+int calcLCM(int n, int m)
+{
+    int lcm = (n * m) / calcGCD(n, m);
+
+    return lcm;
+}
+int main()
+{
+
+    // T.C is:- O(log min(n,m))
+
+    int n, m;
+
+    cout << "Enter The First Number:- " << endl;
+    cin >> n;
+
+    cout << "Enter The Second Number:- " << endl;
+    cin >> m;
+
+    cout << "GCD/HCF between " << n << " and " << m << " is:- ";
+
+    int gcd = calcGCD(n, m);
+
+    cout << gcd << endl;
+
+    cout << "LCM between " << n << " and " << m << " is:- ";
+
+    int lcm = calcLCM(n, m);
+
+    cout << lcm;
+}
