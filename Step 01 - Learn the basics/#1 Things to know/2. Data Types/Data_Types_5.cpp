ce636d44673e6cbef37f2fbd66995f475ad69678
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-03 09:47:37 +0530

    Add example program for character input and output in C++

---
+++ Step 01 - Learn the basics/#1 Things to know/2. Data Types/Data_Types_5.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // char
+
+    char ch = 'm';
+    cout << ch;
+
+    cout << endl;
+
+    char ch1;
+    cin >> ch1;
+
+    cout << ch1;
+}
