688b2ad8f971e9143c774cc48ff06fc4d1a2667b
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-03 10:24:47 +0530

    Add example program demonstrating function pass by reference with integers in C++

---
+++ Step 01 - Learn the basics/#1 Things to know/9. Functions/Function_Pass_By_Reference/Integers.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+// Functions are set of code which performs something for you.
+// Functions are used to modularise code.
+// Functions are used to increase readibility.
+// Functions are used to use same code multiple time.
+
+
+// Pass By Reference (Reference Varibale)
+void doSomething(int &num){ /*Original value comes*/
+
+    /*These all are modification in the original value*/
+    cout<<num<<endl;
+
+    num += 5;
+
+    cout<<num<<endl;
+
+    num += 5;
+
+    cout<<num<<endl;
+}
+
+int main(){
+    int num = 10;
+
+    doSomething(num); /*original number goes*/
+
+    cout<<num<<endl; /*Now it print 20*/
+
+    return 0;
+}
