65df783cd3521eb1de13dadab339bbcd4d7ee087
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-03 11:42:43 +0530

    Pattern-5: Inverted Right Pyramid

---
+++ Step 01 - Learn the basics/#2 Build-up Logical Thinking/Pattern_5.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int row;
+
+    cout << "Enter The Number of Rows: " << endl;
+    cin >> row;
+
+    cout << "Print the Pattern:- " << endl;
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < row - i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    /*
+    for (int i = 1; i <= row; i++)
+   {
+       for (int j = i; j <= row; j++)
+       {
+           cout << "* ";
+       }
+       cout << endl;
+   }*/
+    /*We can also done as:- */
+    /*
+    for(int i=1; i<=row; i++){
+        for(int j=row; j>=i; j--){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    */
+    /*We can also done as:- */
+    /*
+    for(int i=1; i<=row; i++){
+        for(int j=row; j>i-1; j--){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    */
+}
