c85ef2380504547612ca3269bcc9f53caf4f5eec
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-03 11:02:55 +0530

    Add example program demonstrating function pass by reference with float using pointers in C++

---
+++ Step 01 - Learn the basics/#1 Things to know/9. Functions/Function_Pass_By_Reference_With_Pointers/Float.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+// Functions are set of code which performs something for you.
+// Functions are used to modularise code.
+// Functions are used to increase readibility.
+// Functions are used to use same code multiple time.
+
+
+// Pass By Reference with Pointers
+void doSomething(float *num){ /*Original value comes*/
+
+    /*These all are modification in the original value*/
+    cout<<*num<<endl;
+
+    *num += 5.2;
+
+    cout<<*num<<endl;
+
+    *num += 7.15;
+
+    cout<<*num<<endl;
+}
+
+int main(){
+    float num = 10.50;
+
+    doSomething(&num); /*original number address goes*/
+
+    cout<<num<<endl; /*Now it print 22.85*/
+
+    return 0;
+}
