d007d9ad15fbee22a884d9353e245a90b598023e
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-03 17:39:23 +0530

    Add pattern 30: Number Triangle Pattern

---
+++ Step 01 - Learn the basics/#2 Build-up Logical Thinking/Program_10_1.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int row;
+
+    cout << "Enter The Number of Rows: " << endl;
+    cin >> row;
+
+    cout << "Print the Pattern:- " << endl;
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << j + 1;
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < row - 1; i++)
+    {
+        for (int j = 0; j < row - i - 1; j++)
+        {
+            cout << j + 1;
+        }
+        cout << endl;
+    }
+}
