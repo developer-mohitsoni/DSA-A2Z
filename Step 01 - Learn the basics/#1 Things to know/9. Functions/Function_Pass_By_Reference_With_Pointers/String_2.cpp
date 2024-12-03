193a196ecbe073b634a4735409e20c4532d695e9
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-03 11:07:27 +0530

    Add example-2 program demonstrating function pass by reference with pointers using a string in C++

---
+++ Step 01 - Learn the basics/#1 Things to know/9. Functions/Function_Pass_By_Reference_With_Pointers/String_2.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// Functions are set of code which performs something for you.
+// Functions are used to modularise code.
+// Functions are used to increase readibility.
+// Functions are used to use same code multiple time.
+
+// Pass by Reference with Pointers
+void doSomething(string *s)
+{
+
+  cout << *s << endl; // Print the original string object passed in function call.
+
+  string &new_str = *s; // creating a reference variable and pointing it towards the address of value in main memory where "
+
+  new_str[new_str.size() - 2] = 'a';
+  new_str[new_str.size() - 1] = 'n';
+
+  // You can also done this as well
+
+  /*
+  s[s.size() - 1] = "n";
+  s[s.size() - 2] = "a";
+  */
+}
+
+int main()
+{
+  string s = "Mohit";
+
+  doSomething(&s); // Pass a pointer to the string object to the function
+
+  cout << s << endl; // Print the original string object
+
+  return 0;
+}
