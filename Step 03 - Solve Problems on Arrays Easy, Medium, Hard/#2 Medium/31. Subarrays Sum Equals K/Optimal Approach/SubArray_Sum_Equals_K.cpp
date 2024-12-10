0c9ed51852074970863f090bab01bcc8e5fee654
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-10 20:16:50 +0530

    Add C++ program implementing optimal approach to find number of subarrays whose sum equals to K

---
+++ Step 03 - Solve Problems on Arrays Easy, Medium, Hard/#2 Medium/31. Subarrays Sum Equals K/Optimal Approach/SubArray_Sum_Equals_K.cpp
@@ -0,0 +1,59 @@
+//! Program to find Number of Subarrays whose Sum Equals to K
+#include <bits/stdc++.h>
+using namespace std;
+
+//?------------------(Optimal Force Approach)------------------------------
+
+//* T.C = O(N * logN)
+
+//* S.C is O(N).
+
+int subArraySumEqualK(vector<int> &v, int K)
+{
+    int n = v.size();
+
+    map<int, int> mpp;
+
+    mpp[0] = 1;
+
+    int preSum = 0, count = 0;
+
+    // O(N * log N)
+    for (int i = 0; i < n; i++)
+    {
+        preSum += v[i];
+
+        int remove = preSum - K;
+
+        count += mpp[remove];
+
+        mpp[preSum] += 1;
+    }
+
+    return count;
+}
+
+int main()
+{
+
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    int k;
+    cout << "Enter The Sum Equal K Value:- " << endl;
+    cin >> k;
+
+    vector<int> arr(n);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int ans = subArraySumEqualK(arr, k);
+
+    cout << "Number of subarrays whose sum equals to K: " << ans << endl;
+
+    return 0;
+}
