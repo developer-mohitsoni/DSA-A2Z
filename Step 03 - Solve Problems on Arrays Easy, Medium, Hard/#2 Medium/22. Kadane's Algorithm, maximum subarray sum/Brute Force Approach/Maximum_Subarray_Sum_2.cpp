45a248746d618dea2a3ac04e9626c75936836955
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-10 17:08:08 +0530

    Update comment to clarify that the implementation is a brute force approach for maximum subarray sum

--- Step 03 - Solve Problems on Arrays Easy, Medium, Hard/#2 Medium/22. Kadane's Algorithm, maximum subarray sum/Brute Force Approach/Maximum_Subarray_Sum_2.cpp
+++ Step 03 - Solve Problems on Arrays Easy, Medium, Hard/#2 Medium/22. Kadane's Algorithm, maximum subarray sum/Brute Force Approach/Maximum_Subarray_Sum_2.cpp
@@ -2,7 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//?------------------(Better Approach)------------------------------
+//?------------------(Brute Approach)------------------------------
 
 //* T.C = O(N^2)
 
