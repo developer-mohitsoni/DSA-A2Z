f9d749b797e6915017c47735ab6d145807832834
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-10 19:50:18 +0530

    Add C++ program implementing optimal approach to set matrix zeroes

---
+++ Step 03 - Solve Problems on Arrays Easy, Medium, Hard/#2 Medium/28. Set Matrix Zeroes/Optimal Approach/Set_Matrix_Zeroes.cpp
@@ -0,0 +1,103 @@
+//! Program for Set Matrix Zeroes
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//?------------------(Optimal Force Approach)------------------------------
+
+//* T.C = O(2 * (N * M))
+//* S.C is O(1).
+
+void setMatrixZeroes(vector<vector<int>> &matrix, int n, int m)
+{
+    // int row[n] = {0}; : -> matrix[0][..]
+    // int column[m] = {0}; : -> matrix[..][0]
+
+    int col0 = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                // mark the i-th row
+                matrix[i][0] = 0;
+
+                // mark the j-th col
+                if (j != 0)
+                    matrix[0][j] = 0;
+                else
+                    col0 = 0;
+            }
+        }
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j < m; j++)
+        {
+            if (matrix[i][j] != 0)
+            {
+                // check for col & row
+
+                if (matrix[0][j] == 0 || matrix[i][0] == 0)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    if (matrix[0][0] == 0)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matrix[0][j] = 0;
+        }
+    }
+
+    if (col0 == 0)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            matrix[i][0] = 0;
+        }
+    }
+}
+
+int main()
+{
+    int rows;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    int cols;
+    cout << "Enter the number of cols: ";
+    cin >> cols;
+
+    vector<vector<int>> arr(rows, vector<int>(cols));
+
+    cout << "Enter the elements of the array: " << endl;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    setMatrixZeroes(arr, rows, cols);
+
+    cout << "The Final 2D Vector is: " << endl;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl; // To print the next row in a new line
+    }
+
+    return 0;
+}
