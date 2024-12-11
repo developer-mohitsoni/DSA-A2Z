53b925d560387864343266419e105017cd9fe925
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-11 22:16:04 +0530

    Fix loop condition in brute force approach for 3SUM problem

--- Step 03 - Solve Problems on Arrays Easy, Medium, Hard/#3 Hard/34. 3Sum/Brute Force Approach/3SUM.cpp
+++ Step 03 - Solve Problems on Arrays Easy, Medium, Hard/#3 Hard/34. 3Sum/Brute Force Approach/3SUM.cpp
@@ -16,7 +16,7 @@ vector<vector<int>> threeSum(vector<int> &v, int n)
     {
         for (int j = i + 1; j < n - 1; j++)
         {
-            for (int k = j + 1; j < n; j++)
+            for (int k = j + 1; k < n; k++)
             {
                 if (v[i] + v[j] + v[k] == 0)
                 {
