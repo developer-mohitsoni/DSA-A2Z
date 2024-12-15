980d2e62b0960232f0eba971427cba37c3a6c2e6
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-15 19:31:53 +0530

    Add C++ program implementing brute force approach to find maximum product of subarray

---
+++ Step 03 - Solve Problems on Arrays Easy, Medium, Hard/#3 Hard/43. Maximum Product Subarray/Brute Force Approach/Maximum_Product_Subarray.cpp
@@ -0,0 +1,45 @@
+//! Program to Find the Maximum Product of Subarray
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//?------------------(Brute Force Approach)------------------------------
+
+//* T.C is O(n^3)
+
+//* S.C is O(1)
+int maxProductSubArray(vector<int> &nums, int n)
+{
+    int result = INT_MIN;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            int prod = 1;
+            for (int k = i; k <= j; k++)
+                prod *= nums[k];
+            result = max(result, prod);
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter the elements of the first array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int ans = maxProductSubArray(arr, n);
+
+    cout << "The number of inversions are: " << ans << endl;
+
+    return 0;
+}
