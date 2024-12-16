434601331ff7cb717d5389526f3f540028610115
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-16 11:29:49 +0530

    Add implementation of search insert position using STL in C++

---
+++ Step 04 - Binary Search 1D, 2D Arrays, Search Space/Lec_1- BS-on-1D-Arrays/4. Search Insert Position/Optimal Approach/Search_Insert_Position_using_STL.cpp
@@ -0,0 +1,41 @@
+//! Program to Implement Search Insert Position in an array using STL
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//? ---------------------------- Optimal Approach ---------------------------
+
+//* T.C => O(logN)
+//* S.C => O(1)
+
+//! NOTE: Binary Search is works only on sorted arrays
+
+int main()
+{
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int element;
+    cout << "Enter the element to search for: ";
+    cin >> element;
+
+    // lower_bound(arr.begin(), arr.end(), target): Returns an iterator pointing to the first element ≥ target
+
+    // arr.begin(): Iterator pointing to the first element
+
+    // - arr.begin(): Gives the 0-based index of the iterator returned by lower_bound.
+
+    //! NOTE:- The lower_bound function uses binary search internally.
+
+    int ans = lower_bound(arr.begin(), arr.end(), element) - arr.begin(); // Returns iterator pointing to the first occurrence of target in arr or last position where target could be inserted.
+
+    cout << "The element is inserted at position " << ans << endl;
+
+    return 0;
+}
