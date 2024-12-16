fafdd7fa27726e5335e2a8590e35166fbbec8196
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-16 15:25:52 +0530

    Add optimal approach to search for an element in a rotated sorted array

---
+++ Step 04 - Binary Search 1D, 2D Arrays, Search Space/Lec_1- BS-on-1D-Arrays/8. Search Element in Rotated and Sorted Array/Optimal Approach/Search_Element_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,78 @@
+//! Program to Search Element in Rotated and Sorted Array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//? ---------------------------- Optimal Approach ---------------------------
+
+//* T.C => O(logN)
+//* S.C => O(1)
+
+//! NOTE: Binary Search is works only on sorted arrays
+
+int search(vector<int> &arr, int n, int element)
+{
+    int low = 0, high = n - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        // if mid points the target
+        if (arr[mid] == element)
+            return mid;
+
+        // if left part is sorted:
+        if (arr[low] <= arr[mid])
+        {
+            if (arr[low] <= element && element <= arr[mid])
+            {
+                // element exists:
+                high = mid - 1;
+            }
+            else
+            {
+                // element does not exist:
+                low = mid + 1;
+            }
+        }
+        else
+        { // if right part is sorted:
+            if (arr[mid] <= element && element <= arr[high])
+            {
+                // element exists:
+                low = mid + 1;
+            }
+            else
+            {
+                // element does not exist:
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int element;
+    cout << "Enter the element to find: ";
+    cin >> element;
+
+    int ans = search(arr, n, element);
+
+    if (ans == -1)
+        cout << "Element not found in the array" << endl;
+    else
+        cout << "Element found at index: " << ans << endl;
+
+    return 0;
+}
