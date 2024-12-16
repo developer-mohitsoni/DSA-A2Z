6d5c70873f0e7c88c1f08e625187da6be92f36c2
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-16 11:45:47 +0530

    Add implementation of floor and ceil in sorted array using brute force approach in C++

---
+++ Step 04 - Binary Search 1D, 2D Arrays, Search Space/Lec_1- BS-on-1D-Arrays/5. Floor and Ceil in Sorted Array/Optimal Approach/Floor_and_Ceil_in_Sorted_Array.cpp
@@ -0,0 +1,86 @@
+//! Program to find Floor and Ceil in Sorted Array using Brute Force Approach
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//? ---------------------------- Brute Force Approach ---------------------------
+
+//* T.C => O(logN)
+//* S.C => O(1)
+
+//! NOTE: Binary Search is works only on sorted arrays
+
+int findFloor(vector<int> &arr, int n, int value)
+{
+    int low = 0, high = n - 1;
+    int ans = -1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        // maybe an answer
+        if (arr[mid] <= value)
+        {
+            ans = arr[mid];
+            // look for smaller index on the left
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1; // look on the right
+        }
+    }
+    return ans;
+}
+
+int findCeil(vector<int> &arr, int n, int value)
+{
+    int low = 0, high = n - 1;
+    int ans = -1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        // maybe an answer
+        if (arr[mid] >= value)
+        {
+            ans = arr[mid];
+            // look for smaller index on the left
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+
+pair<int, int> getFloorAndCeil(vector<int> &arr, int n, int value)
+{
+    int f = findFloor(arr, n, value);
+    int c = findCeil(arr, n, value);
+    return make_pair(f, c);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int element;
+    cout << "Enter the element to find its floor and ceil: ";
+    cin >> element;
+
+    pair<int, int> ans = getFloorAndCeil(arr, n, element);
+
+    cout << "The floor and ceil of " << element << " is at index: " << ans.first << " and " << ans.second << endl;
+
+    return 0;
+}
