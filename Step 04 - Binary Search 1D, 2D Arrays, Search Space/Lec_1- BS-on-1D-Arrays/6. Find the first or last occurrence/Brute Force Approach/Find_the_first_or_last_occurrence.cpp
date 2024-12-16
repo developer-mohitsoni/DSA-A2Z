8d50d18610676cd197b38fcf834a9bd4cabb08b4
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-16 12:18:52 +0530

    Fix loop condition in brute force approach for finding first and last occurrence

--- Step 04 - Binary Search 1D, 2D Arrays, Search Space/Lec_1- BS-on-1D-Arrays/6. Find the first or last occurrence/Brute Force Approach/Find_the_first_or_last_occurrence.cpp
+++ Step 04 - Binary Search 1D, 2D Arrays, Search Space/Lec_1- BS-on-1D-Arrays/6. Find the first or last occurrence/Brute Force Approach/Find_the_first_or_last_occurrence.cpp
@@ -14,7 +14,7 @@ pair<int, int> firstAndLastOccurrence(vector<int> &arr, int n, int element)
 {
     int first = -1, last = -1;
 
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i < n; i++)
     {
         if (arr[i] == element)
         {
