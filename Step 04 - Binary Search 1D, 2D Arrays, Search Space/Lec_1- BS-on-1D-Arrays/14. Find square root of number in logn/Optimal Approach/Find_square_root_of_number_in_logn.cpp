2f7953e02c2cc8d689729b75b90d9949f50f0af3
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-17 11:58:51 +0530

    Add optimal implementation to find the square root of a number in O(log N)

---
+++ Step 04 - Binary Search 1D, 2D Arrays, Search Space/Lec_1- BS-on-1D-Arrays/14. Find square root of number in logn/Optimal Approach/Find_square_root_of_number_in_logn.cpp
@@ -0,0 +1,32 @@
+//! Program to find the square root of number in logn
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//? ---------------------------- Brute Force Approach ---------------------------
+
+//* T.C => O(logN)
+//* S.C => O(1)
+
+//! NOTE: Binary Search is works only on sorted arrays
+
+int floorSqrt(int n)
+{
+    int ans = sqrt(n);
+
+    return ans;
+}
+
+int main()
+{
+    int number;
+    cout << "Enter the number to find the perfect square root: "
+         << endl;
+    cin >> number;
+
+    int ans = floorSqrt(number);
+
+    cout << "The floor of perfect square root of " << number << " is: " << ans << endl;
+
+    return 0;
+}
