3b3a35d13da0204c884c0457d5a1a923a45e24b8
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-18 12:17:08 +0530

    Add brute force approach to find the min. distance between the cows such that the min. distance is Maximum (Aggressive Cows)

---
+++ Step 04 - Binary Search 1D, 2D Arrays, Search Space/Lec_2- BS-on-Answers/21. Aggressive Cows/Brute Force Approach/Aggressive_Cows.cpp
@@ -0,0 +1,70 @@
+//! Program the find the min. distance between the cows such that the min. distance is Maximum (Aggressive Cows)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//? ---------------------------- Brute Force Approach ---------------------------
+
+//* T.C => O(max-min)*O(N)
+
+//* S.C => O(1)
+
+//! NOTE: Binary Search is works only on sorted arrays
+
+bool canWePlace(vector<int> &stalls, int dist, int cows)
+{
+    int n = stalls.size(); // size of array
+    int cntCows = 1;       // no. of cows placed
+    int last = stalls[0];  // position of last placed cow.
+    for (int i = 1; i < n; i++)
+    {
+        if (stalls[i] - last >= dist)
+        {
+            cntCows++;        // place next cow.
+            last = stalls[i]; // update the last location.
+        }
+        if (cntCows >= cows)
+            return true;
+    }
+    return false;
+}
+
+int aggressiveCows(vector<int> &stalls, int k)
+{
+    int n = stalls.size();
+
+    sort(stalls.begin(), stalls.end());
+
+    int limit = stalls[n - 1] - stalls[0];
+
+    for (int i = 1; i <= limit; i++)
+    {
+        if (canWePlace(stalls, i, k) == false)
+        {
+            return (i - 1); // return the previous
+        }
+    }
+    return limit;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+
+    vector<int> stalls(n);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+        cin >> stalls[i];
+
+    int k;
+    cout << "Enter the number of cows: ";
+    cin >> k;
+
+    int ans = aggressiveCows(stalls, k);
+
+    cout << "The maximum possible minimum distance is: " << ans << "\n";
+
+    return 0;
+}
