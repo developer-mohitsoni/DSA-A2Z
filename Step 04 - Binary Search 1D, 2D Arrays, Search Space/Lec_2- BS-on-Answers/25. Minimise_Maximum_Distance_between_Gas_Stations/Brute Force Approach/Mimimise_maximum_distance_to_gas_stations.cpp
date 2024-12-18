6f2cc9c31a7168f3025e5b0b8c670dcb194ae62a
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-18 15:44:19 +0530

    Add a brute force approach Minimise the Maximum distance between the gas stations.

---
+++ Step 04 - Binary Search 1D, 2D Arrays, Search Space/Lec_2- BS-on-Answers/25. Minimise_Maximum_Distance_between_Gas_Stations/Brute Force Approach/Mimimise_maximum_distance_to_gas_stations.cpp
@@ -0,0 +1,75 @@
+//! Program of Minimise the Maximum distance between the gas stations.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//? ---------------------------- Brute Force Approach ---------------------------
+
+//* T.C =>  O(N * (sum(arr[])-max(arr[])+1))
+
+//* S.C => O(1)
+
+//! NOTE: Binary Search is works only on sorted arrays
+
+long double minimiseMaxDistance(vector<int> &arr, int k)
+{
+    int n = arr.size();
+
+    vector<int> howMany(n - 1, 0);
+
+    // Pick and place k gas stations:
+
+    for (int gasStations = 1; gasStations <= k; gasStations++)
+    {
+        // Find the maximum section
+        // and insert the gas station:
+
+        long double maxSection = -1;
+        int maxInd = -1;
+        for (int i = 0; i < n - 1; i++)
+        {
+            long double diff = arr[i + 1] - arr[i];
+            long double sectionLength =
+                diff / (long double)(howMany[i] + 1);
+            if (sectionLength > maxSection)
+            {
+                maxSection = sectionLength;
+                maxInd = i;
+            }
+        }
+        // insert the current gas station:
+        howMany[maxInd]++;
+    }
+    // Find the maximum distance i.e. the answer:
+    long double maxAns = -1;
+    for (int i = 0; i < n - 1; i++)
+    {
+        long double diff = arr[i + 1] - arr[i];
+        long double sectionLength =
+            diff / (long double)(howMany[i] + 1);
+        maxAns = max(maxAns, sectionLength);
+    }
+    return maxAns;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int k;
+    cout << "Enter the number of gas stations: ";
+    cin >> k;
+
+    long double ans = minimiseMaxDistance(arr, k);
+
+    cout << "Minimum maximum distance between gas stations: " << fixed << setprecision(2) << ans << endl;
+
+    return 0;
+}
