c4aebb44d9cc676bfad6791fca83d0fa74b49192
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-19 15:35:02 +0530

    Add a optimal approach to Search in a 2D Matrix using Binary Search

---
+++ Step 04 - Binary Search 1D, 2D Arrays, Search Space/Lec_3- BS-on-2D-Arrays/29. Search in a 2D Matrix/Optimal Approach/Search_in_a_2D_Matrix.cpp
@@ -0,0 +1,68 @@
+//! Program to Search in a 2D Matrix Leetcode:- 74
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//?------------------(Better Force Approach)------------------------------
+
+//* T.C = O(log(NxM))
+
+// Reason => We are applying binary search on the imaginary 1D array of size NxM.
+
+//* S.C is O(1).
+
+bool searchMatrix(vector<vector<int>> &matrix, int target)
+{
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    // apply binary search:
+    int low = 0, high = n * m - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        int row = mid / m, col = mid % m;
+        if (matrix[row][col] == target)
+            return true;
+        else if (matrix[row][col] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return false;
+}
+
+int main()
+{
+    int rows;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    int cols;
+    cout << "Enter the number of cols: ";
+    cin >> cols;
+
+    vector<vector<int>> matrix(rows, vector<int>(cols));
+
+    cout << "Enter the elements of the array: " << endl;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+
+    int target;
+    cout << "Enter the target element: ";
+    cin >> target;
+
+    bool found = searchMatrix(matrix, target);
+
+    if (found)
+        cout << "Element found in the matrix" << endl;
+    else
+        cout << "Element not found in the matrix" << endl;
+
+    return 0;
+}
