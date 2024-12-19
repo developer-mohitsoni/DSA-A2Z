f5302d15c5c02ae8dd86233297b13035cee9297e
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-19 15:41:37 +0530

    Add a better approach to Search in a 2D Matrix-II using Binary Search

---
+++ Step 04 - Binary Search 1D, 2D Arrays, Search Space/Lec_3- BS-on-2D-Arrays/30. Search in a 2D Matrix-II/Better Approach/Search_in_a_2D_Matrix.cpp
@@ -0,0 +1,79 @@
+//! Program to Search in a 2D Matrix-II Leetcode:- 240
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//?------------------(Better Force Approach)------------------------------
+
+//* T.C = O(N + logM)
+
+// Reason => We are traversing all rows and it takes O(N) time complexity. But for all rows, we are not applying binary search rather we are only applying it once for a particular row. That is why the time complexity is O(N + logM) instead of O(N*logM).
+
+//* S.C is O(1).
+
+bool binarySearch(vector<int> &nums, int target)
+{
+    int n = nums.size(); // size of the array
+    int low = 0, high = n - 1;
+
+    // Perform the steps:
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target)
+            return true;
+        else if (target > nums[mid])
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return false;
+}
+
+bool searchMatrix(vector<vector<int>> &matrix, int target)
+{
+    int n = matrix.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        bool flag = binarySearch(matrix[i], target);
+        if (flag)
+            return true;
+    }
+    return false;
+}
+
+int main()
+{
+    int rows;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    int cols;
+    cout << "Enter the number of cols: ";
+    cin >> cols;
+
+    vector<vector<int>> matrix(rows, vector<int>(cols));
+
+    cout << "Enter the elements of the array: " << endl;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+
+    int target;
+    cout << "Enter the target element: ";
+    cin >> target;
+
+    bool found = searchMatrix(matrix, target);
+
+    if (found)
+        cout << "Element found in the matrix" << endl;
+    else
+        cout << "Element not found in the matrix" << endl;
+
+    return 0;
+}
