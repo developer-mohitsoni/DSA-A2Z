ed807e6b5f5be77e88430a4619ff91ab7df124a3
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-19 15:20:58 +0530

    Add a brute force approach to Search in a 2D Matrix

---
+++ Step 04 - Binary Search 1D, 2D Arrays, Search Space/Lec_3- BS-on-2D-Arrays/29. Search in a 2D Matrix/Brute Force Approach/Search_in_a_2D_Matrix.cpp
@@ -0,0 +1,60 @@
+//! Program to Search in a 2D Matrix Leetcode:- 74
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//?------------------(Brute Force Approach)------------------------------
+
+//* T.C = O(N*M)
+//* S.C is O(1).
+
+bool searchMatrix(vector<vector<int>> &matrix, int target)
+{
+    int n = matrix.size(), m = matrix[0].size();
+
+    // traverse the matrix:
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (matrix[i][j] == target)
+                return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    int rows;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    int cols;
+    cout << "Enter the number of cols: ";
+    cin >> cols;
+
+    vector<vector<int>> matrix(rows, vector<int>(cols));
+
+    cout << "Enter the elements of the array: " << endl;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+
+    int target;
+    cout << "Enter the target element: ";
+    cin >> target;
+
+    bool found = searchMatrix(matrix, target);
+
+    if (found)
+        cout << "Element found in the matrix" << endl;
+    else
+        cout << "Element not found in the matrix" << endl;
+
+    return 0;
+}
