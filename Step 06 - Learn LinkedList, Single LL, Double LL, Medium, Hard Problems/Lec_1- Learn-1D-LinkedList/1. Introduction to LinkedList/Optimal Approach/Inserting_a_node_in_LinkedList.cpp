af5429a1ecd292ec06a06a00a1a25a02319c9d14
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-20 16:34:15 +0530

    Program to Inserting a Node in Linked List

---
+++ Step 06 - Learn LinkedList, Single LL, Double LL, Medium, Hard Problems/Lec_1- Learn-1D-LinkedList/1. Introduction to LinkedList/Optimal Approach/Inserting_a_node_in_LinkedList.cpp
@@ -0,0 +1,65 @@
+//! Into to Linked List Code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//? ---------------------------- Iterative Approach ---------------------------
+
+//* Memory Space:-
+
+/*
+*            32 Bit System               64 Bit System
+
+*            Int - 4 Bytes               Int - 4 Bytes
+*            Pointer - 4 Bytes           Pointer - 8 Bytes
+*            Overall - 8 Bytes           Overall - 12 Byte
+*/
+class Node
+{
+public:
+    int data;   // Data stored in the node
+    Node *next; // Pointer to the next node in the linked list
+
+    // Constructors
+public:
+    Node(int data1, Node *next)
+    {
+        data = data1;
+        next = next;
+    }
+
+public:
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr; // nullptr
+    }
+};
+
+int main()
+{
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    // Creating the linked list from user input
+    Node *head = new Node(arr[0]);
+    Node *current = head;
+    for (int i = 1; i < n; i++)
+    {
+        current->next = new Node(arr[i]);
+        current = current->next;
+    }
+
+    cout << "head->data => " << head->data << endl;
+    cout << "head->next->data => " << head->next->data << endl;
+    cout << "head->next->next->data => " << head->next->next->data
+         << endl;
+
+    return 0;
+}
