d8d3cc07c481cd921e019b089e8f9b17dab560cb
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-20 16:48:02 +0530

    Program to Find the Length of the Linked List

---
+++ Step 06 - Learn LinkedList, Single LL, Double LL, Medium, Hard Problems/Lec_1- Learn-1D-LinkedList/1. Introduction to LinkedList/Optimal Approach/4. Length_of_a_LinkedList.cpp
@@ -0,0 +1,96 @@
+//! Program to Find the Length of the Linked List
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//? ---------------------------- Iterative Approach ---------------------------
+
+//* T.C => O(N) => for traversal in the linked list + O(N) => for returning the length of the linked list => O(N) + O(N) => O(2N) => O(N)
+
+//* S.C => O(1) => constant space complexity
+
+//* Memory Space:-
+
+/*
+*            32 Bit System               64 Bit System
+
+*            Int - 4 Bytes               Int - 4 Bytes
+*            Pointer - 4 Bytes           Pointer - 8 Bytes
+*            Overall - 8 Bytes           Overall - 12 Byte
+*/
+class Node
+{
+public:
+    int data;   // Data stored in the node
+    Node *next; // Pointer to the next node in the linked list
+
+    // Constructors
+public:
+    Node(int data1, Node *next)
+    {
+        data = data1;
+        next = next;
+    }
+
+public:
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr; // nullptr
+    }
+};
+
+// Function to print the linked list
+void printLL(Node *head)
+{
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int lengthLL(Node *head)
+{
+    int count = 0;
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    // Creating the linked list from user input
+    Node *head = new Node(arr[0]);
+    Node *current = head;
+    for (int i = 1; i < n; i++)
+    {
+        current->next = new Node(arr[i]);
+        current = current->next;
+    }
+
+    // Printing the linked list
+    cout << "Linked list after insertion: ";
+    printLL(head);
+
+    // Finding the length of the linked list
+    int length = lengthLL(head);
+    cout << "Length of the linked list: " << length << endl;
+
+    return 0;
+}
