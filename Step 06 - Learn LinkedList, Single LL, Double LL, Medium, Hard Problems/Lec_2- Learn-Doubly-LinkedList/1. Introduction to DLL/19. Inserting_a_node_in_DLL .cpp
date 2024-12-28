fc628550d26a23f13b9331bc69ef2760a49a2d66
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-28 19:27:19 +0530

    Program to Inserting a Node in Linked List

---
+++ Step 06 - Learn LinkedList, Single LL, Double LL, Medium, Hard Problems/Lec_2- Learn-Doubly-LinkedList/1. Introduction to DLL/19. Inserting_a_node_in_DLL .cpp
@@ -0,0 +1,73 @@
+//! Program to Inserting a Node in Linked List
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//? ---------------------------- Iterative Approach ---------------------------
+
+//* T.C => O(N) => to inserting every element in the linked list from the array.
+
+//* S.C => O(1) => constant space complexity
+
+//* Memory Space:-
+
+/*
+*            32 Bit System               64 Bit System
+
+*            Int - 4 Bytes               Int - 4 Bytes
+*            Pointer - 4 Bytes           Pointer - 8 Bytes
+*            Overall - 8 Bytes           Overall - 12 Byte
+*/
+class Node
+{
+public:
+    Node *back; // Pointer to the previous node in the DLL
+    int data;   // Data stored in the node
+    Node *next; // Pointer to the next node in the DLL
+
+    // Constructors
+    Node(int data1, Node *next, Node *back)
+    {
+        this->back = back;
+        this->data = data1;
+        this->next = next;
+    }
+
+    Node(int data1)
+    {
+        this->back = nullptr; // Pointer to the previous node in the DLL
+        this->data = data1;
+        this->next = nullptr; // Pointer to the next node in the DLL
+    }
+};
+
+int main()
+{
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+
+    // Check for valid input size
+    if (n <= 0)
+    {
+        cout << "Array size must be greater than 0." << endl;
+        return 0;
+    }
+
+    vector<int> arr(n);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    // Creating the linked list from user input
+    Node *head = new Node(arr[0]);
+    Node *current = head;
+    for (int i = 1; i < n; i++)
+    {
+        current->next = new Node(arr[i]);
+        current->next->back = current; // Correctly assign the back pointer
+        current = current->next;
+    }
+
+    return 0;
+}
