a41ab94df47b364570e94c65000bcf15845e046f
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-28 19:54:39 +0530

    Modified Code

--- Step 06 - Learn LinkedList, Single LL, Double LL, Medium, Hard Problems/Lec_2- Learn-Doubly-LinkedList/1. Introduction to DLL/19. Inserting_a_node_in_DLL .cpp
+++ Step 06 - Learn LinkedList, Single LL, Double LL, Medium, Hard Problems/Lec_2- Learn-Doubly-LinkedList/1. Introduction to DLL/19. Inserting_a_node_in_DLL .cpp
@@ -61,12 +61,14 @@ int main()
 
     // Creating the linked list from user input
     Node *head = new Node(arr[0]);
-    Node *current = head;
+    Node *prev = head;
     for (int i = 1; i < n; i++)
     {
-        current->next = new Node(arr[i]);
-        current->next->back = current; // Correctly assign the back pointer
-        current = current->next;
+        Node *temp = new Node(arr[i], nullptr, prev);
+
+        prev->next = temp;
+
+        prev = temp;
     }
 
     return 0;
