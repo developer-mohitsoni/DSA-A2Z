2cc1660f8f5a2428915acce1f8523a767bbcc726
Mohit Soni <developer_mohitsoni@outlook.com>
2025-02-23 19:35:40 +0530

    Refactor Returning_Function_Character.cpp to improve code formatting and enhance readability

--- Step 01 - Learn the basics/#1 Things to know/9. Functions/Function_Pass_By_Reference/Returning Function (Pass by Reference)/Returning_Function_Character.cpp
+++ Step 01 - Learn the basics/#1 Things to know/9. Functions/Function_Pass_By_Reference/Returning Function (Pass by Reference)/Returning_Function_Character.cpp
@@ -1,11 +1,10 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 // Functions are set of code which performs something for you.
 // Functions are used to modularise code.
 // Functions are used to increase readibility.
 // Functions are used to use same code multiple time.
-
 
 // Returning Function Pass By Reference (Returning Character)
 
@@ -14,11 +13,13 @@ The error in the code is that the doSomething() function returns a copy of the n
 
 To fix this error, the doSomething() function should return a reference to the num parameter, instead of a copy of the parameter. This can be done by declaring the num parameter as a reference in the function declaration.
 */
-char &doSomething(char &ch){ /*Reference comes*/
+char &doSomething(char &ch)
+{ /*Reference comes*/
 
     /*These all are modification in the original character*/
 
-    if(ch < 'm' && ch < 'M'){
+    if (ch < 'm' && ch < 'M')
+    {
         ch += 5;
         return ch;
     }
@@ -26,14 +27,15 @@ char &doSomething(char &ch){ /*Reference comes*/
     return ch;
 }
 
-int main(){
+int main()
+{
     char ch;
 
-    cin>>ch;
+    cin >> ch;
 
-    char store = doSomething(ch); /* original character address goes*/
+    doSomething(ch) = 'M'; // ch ka reference return ho raha hai, toh ch = M ho jayega
 
-    cout<<store<<endl; /*Print Depend on the Input value*/
-    
+    cout << ch << endl;
+
     return 0;
 }
