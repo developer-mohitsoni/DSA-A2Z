33f311df5ef93f9c9dbec853243fea52fe214eba
Mohit Soni <developer_mohitsoni@outlook.com>
2025-02-23 17:48:00 +0530

    Add string data type demonstration to Data_Types.cpp

--- Step 01 - Learn the basics/#1 Things to know/2. Data Types/Data_Types.cpp
+++ Step 01 - Learn the basics/#1 Things to know/2. Data Types/Data_Types.cpp
@@ -4,35 +4,40 @@ using namespace std;
 
 int main()
 {
-    // int
-    int x = 10;
-    cout << "Value of x: " << x << endl;
-    cout << "Size of int: " << sizeof(int) << endl
-         << endl;
+     // int
+     int x = 10;
+     cout << "Value of x: " << x << endl;
+     cout << "Size of int: " << sizeof(int) << endl
+          << endl;
 
-    // long
-    long y = 15;
-    cout << "Value of y: " << y << endl;
-    cout << "Size of long: " << sizeof(long) << endl
-         << endl;
+     // long
+     long y = 15;
+     cout << "Value of y: " << y << endl;
+     cout << "Size of long: " << sizeof(long) << endl
+          << endl;
 
-    // long long
-    long long z = 150000000;
-    cout << "Value of z: " << z << endl;
-    cout << "Size of long long: " << sizeof(long long) << endl
-         << endl;
+     // long long
+     long long z = 150000000;
+     cout << "Value of z: " << z << endl;
+     cout << "Size of long long: " << sizeof(long long) << endl
+          << endl;
 
-    // float
-    float p = 5.6;
-    cout << "Value of p: " << p << endl;
-    cout << "Size of float: " << sizeof(float) << endl
-         << endl;
+     // float
+     float p = 5.6;
+     cout << "Value of p: " << p << endl;
+     cout << "Size of float: " << sizeof(float) << endl
+          << endl;
 
-    // double
-    double q = 5;
-    cout << "Value of q: " << q << endl;
-    cout << "Size of double: " << sizeof(double) << endl
-         << endl;
+     // double
+     double q = 5;
+     cout << "Value of q: " << q << endl;
+     cout << "Size of double: " << sizeof(double) << endl
+          << endl;
 
-    return 0;
+     string s = "Hello World";
+     cout << "Value of s: " << s << endl;
+     cout << "Size of string: " << sizeof(string) << endl
+          << endl;
+
+     return 0;
 }
