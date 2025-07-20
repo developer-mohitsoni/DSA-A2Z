b7755061ef4301e6256fe24b8ad414130666f7c7
Mohit Soni <developer_mohitsoni@outlook.com>
2025-07-20 18:31:41 +0530

    Fix breakpoint calculation in pattern printing logic

--- Step 01 - Learn the basics/#2 Build-up Logical Thinking/Pattern_17.cpp
+++ Step 01 - Learn the basics/#2 Build-up Logical Thinking/Pattern_17.cpp
@@ -17,7 +17,7 @@ int main(){
         }
         // alpha
         char ch = 'A';
-        int breakPoint = (2*i+1)/2;
+        int breakPoint = i;
 
         for(int j=1; j<=2*i+1; j++){
             cout<<ch;
