540410754dd21ec6599897cc3f2f2238263c62b5
Mohit Soni <developer_mohitsoni@outlook.com>
2025-07-22 22:05:19 +0530

    Fix loop boundaries in selection sort implementation for correctness

--- Step 02 - Learn Important Sorting Techniques/#1  Sorting-I/1. Selection Sort/Selection_Sort.cpp
+++ Step 02 - Learn Important Sorting Techniques/#1  Sorting-I/1. Selection Sort/Selection_Sort.cpp
@@ -8,11 +8,11 @@ using namespace std;
 
 void selectionSort(int arr[], int n)
 {
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i <= n - 2; i++)
     {
         int minn = i;
 
-        for (int j = i + 1; j < n; j++)
+        for (int j = i + 1; j <= n-1; j++)
         {
             if (arr[j] < arr[minn])
                 minn = j;
